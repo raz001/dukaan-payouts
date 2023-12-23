ddc14e8c95ed425cd22b64a1b08926695fde418f
raz001 <razbhagat1@gmail.com>
2023-12-23 10:47:25 +0530

    payouts table added

--- src/components/Payouts.jsx
+++ src/components/Payouts.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Overview from './Overview'
+import Transaction from './Transaction'
 
 const Payouts = () => {
   return (
     <section class="payouts">
         <Header />
         <Overview />
-        
+        <Transaction />
     </section>
   )
 }
